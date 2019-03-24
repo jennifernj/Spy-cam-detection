900a6bf2e4ec4126366021d4d59bf30fd892b533
jennifernj <jenniferneda@gmail.com>
2019-03-24 02:26:23 -0700

    cool loud buzzer things

--- spy_cam_detection.ino
+++ spy_cam_detection.ino
@@ -4,58 +4,9 @@ const int pinAdc = A0;
 const int pinLight = A3;
 const int buttonPin = 2;
 const int ledPin=11;
-int buttonState = 0;
-QuickStats stats;
-
-void setup()
-{
-    pinMode(ledPin, OUTPUT);
-    pinMode(buttonPin, INPUT);
-    Serial.begin(9600);
-}
-
-void loop()
-{
-    float sounds[10];
-    float lights[10];
-    for (int j=0; j<10; j++){
-      buttonState = digitalRead(buttonPin);
-      long sum = 0;
-      for(int i=0; i<32; i++)
-      {
-          sum += analogRead(pinAdc);
-      }
-  
-      sum >>= 5;
-  
-      int light = analogRead(A3);
-      Serial.print(light);
-      Serial.print(",");
-      Serial.println(sum);
-      if (buttonState == HIGH) {
-        digitalWrite(ledPin, HIGH);
-      }
-      else {
-        digitalWrite(ledPin, LOW);
-      }
-      delay(10);
-      sounds[j]=sum;
-      lights[j]=light;
-    }
-
-    float soundStd=stats.stdev(sounds,10);
-    float lightStd=stats.stdev(lights,10);
-    float soundAvg=stats.average(sounds,10);
-    float lightAvg=stats.average(lights,10);
-    
-}#include "QuickStats.h"
-
-const int pinAdc = A0;
-const int pinLight = A3;
-const int buttonPin = 2;
-const int ledPin=11;
 const int buzzerPin = A1;
 int buttonState = 0;
+const int interval = 50;
 QuickStats stats;
 
 void setup()
@@ -68,9 +19,9 @@ void setup()
 
 void loop()
 {
-    float sounds[10];
-    float lights[10];
-    for (int j=0; j<10; j++){
+    float sounds[interval];
+    float lights[interval];
+    for (int j=0; j<interval; j++){
       buttonState = digitalRead(buttonPin);
       long sum = 0;
       for(int i=0; i<32; i++)
@@ -95,14 +46,20 @@ void loop()
       lights[j]=light;
     }
 
-    digitalWrite(buzzerPin, HIGH);
-    delay(1000);
-    digitalWrite(buzzerPin, LOW);
-    delay(1000);
+    float soundStd=stats.stdev(sounds,interval);
+    float lightStd=stats.stdev(lights,interval);
+    float soundAvg=stats.average(sounds,interval);
+    float lightAvg=stats.average(lights,interval);
 
-    float soundStd=stats.stdev(sounds,10);
-    float lightStd=stats.stdev(lights,10);
-    float soundAvg=stats.average(sounds,10);
-    float lightAvg=stats.average(lights,10);
+    int light = analogRead(A3);
+    int sound = analogRead(pinAdc);
+
+    if ((sound>soundStd+soundAvg) && (light>lightStd+lightAvg)) {
+      digitalWrite(buzzerPin, HIGH);
+    }
+    else {
+      digitalWrite(buzzerPin, LOW);
+    }
+    
     
 }
