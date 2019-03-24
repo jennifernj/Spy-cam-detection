6c9e1e25dfd29a65bba7ba412b9318058bd8bb27
jennifernj <jenniferneda@gmail.com>
2019-03-24 01:19:18 -0700

    yay

--- spy_cam_detection.ino
+++ spy_cam_detection.ino
@@ -1,8 +1,11 @@
+#include "QuickStats.h"
+
 const int pinAdc = A0;
 const int pinLight = A3;
 const int buttonPin = 2;
 const int ledPin=11;
 int buttonState = 0;
+QuickStats stats;
 
 void setup()
 {
@@ -13,24 +16,36 @@ void setup()
 
 void loop()
 {
-    buttonState = digitalRead(buttonPin);
-    long sum = 0;
-    for(int i=0; i<32; i++)
-    {
-        sum += analogRead(pinAdc);
+    float sounds[10];
+    float lights[10];
+    for (int j=0; j<10; j++){
+      buttonState = digitalRead(buttonPin);
+      long sum = 0;
+      for(int i=0; i<32; i++)
+      {
+          sum += analogRead(pinAdc);
+      }
+  
+      sum >>= 5;
+  
+      int light = analogRead(A3);
+      Serial.print(light);
+      Serial.print(",");
+      Serial.println(sum);
+      if (buttonState == HIGH) {
+        digitalWrite(ledPin, HIGH);
+      }
+      else {
+        digitalWrite(ledPin, LOW);
+      }
+      delay(10);
+      sounds[j]=sum;
+      lights[j]=light;
     }
 
-    sum >>= 5;
-
-    int light = analogRead(A3);
-    Serial.print(light);
-    Serial.print(",");
-    Serial.println(sum);
-    if (buttonState == HIGH) {
-      digitalWrite(ledPin, HIGH);
-    }
-    else {
-      digitalWrite(ledPin, LOW);
-    }
-    delay(10);
+    float soundStd=stats.stdev(sounds,10);
+    float lightStd=stats.stdev(lights,10);
+    float soundAvg=stats.average(sounds,10);
+    float lightAvg=stats.average(lights,10);
+    
 }
